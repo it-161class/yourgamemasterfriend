ccb3a58e6d1978301edda0d4fcbb7b5d0266f2ce
Zeneak <Joshua.Zimmerschied@gmail.com>
2021-06-13 11:00:43 -0700

    Updating big assignment

--- yourgamemasterfriend.com/it161/big/includes/contact.php
+++ yourgamemasterfriend.com/it161/big/includes/contact.php
@@ -5,22 +5,28 @@
   <meta name="robots" content="noindex,nofollow" />
   <meta name="viewport" content="width=device-width" />
   <meta charset="utf-8" />
-  <link rel="stylesheet" href="css/styles.css"/>
+  <link rel="stylesheet" href="css/portal.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/forms.css" />
  </head>
  <body>
-    <header class="container">
-      <div class="banner">
-        <a href="#top"><img src="images/sample.png" alt="sample"></a>
-        <h1 class="centered">Really Great Builds</h1>
-      </div>
-        <nav>
-        <a href="">part 1</a>
-        <a href="">part 2</a>
-        <a href="contact.php">Contact RGB</a>
-        </nav>
-    </header>
+     <header class="container">
+     <h1>Joshua Zimmerschied's IT161 Website</h1>
+     <nav class="topnav" id="myTopnav">
+       <a href="index.html" class="active">Welcome</a>
+       <a href="elements.html">HTML Elements</a>
+       <a href="aia.html">AIA</a>
+       <a href="big/index.html">BIG</a>
+       <a href="http://w3schools.com" target="_blank">W3 Schools</a>
+       <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
+       <a href="flowchart.html">Flowchart</a>
+       <a href="fp/index.html">Final Project</a>
+       <a href="contact.php">Contact Z</a>
+       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+     </nav>
+     </header>
      
-    <main class="wrapper container">
+  <main class="wrapper container">
     <h2 class="subheader">Contact Z</h2>
     <?php
         /*
